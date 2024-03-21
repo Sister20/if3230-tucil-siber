5193267b8af28030f5596a1b4173c5b1c8211a09
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-03-21 21:21:35 +0700

    feat: add backward elimination
    Co-Authored-By: Ditra Amadia <60308058+ditramadia@users.noreply.github.com>

--- src/open-mpi/mpi.cpp
+++ src/open-mpi/mpi.cpp
@@ -1,46 +1,40 @@
 #include <algorithm>
 #include <iostream>
 #include <memory>
-#include <mpi.h>
+
+#include "mpi.h"
 
 int main(int argc, char *argv[])
 {
-    // Initialize the MPI environment
+    // Initialize MPI
     MPI_Init(&argc, &argv);
 
-    // Get the number of processes
-    int num_tasks;
-    MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
+    // Get the total number of processes
+    int num_processes;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
 
-    // Get the rank of the process
-    int process_id;
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    // Get current rank (Process ID)
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Get dimension of matrix
+    // Get the dimension of the matrix
     int dim;
-    if (process_id == 0)
+    if (rank == 0)
     {
         std::cin >> dim;
     }
 
-    // Broadcast the dimension of the matrix
+    // Broadcast the dimension of the matrix to all processes
     MPI_Bcast(&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Calculate the number of rows each task will handle
-    const int n_rows = dim / num_tasks;
+    // Calculate the number of rows for each matrix chunk
+    const int chunk_rows = dim / num_processes;
 
-    // Variable to store the chunk of the matrix
-    auto m_chunk = std::make_unique<double[]>(dim * n_rows * 2);
-
-    // Variable to store the pivot row of each iteration
-    auto pivot_row = std::make_unique<double[]>(dim * 2);
-
-    // Read in the matrix
+    // Read matrix from text file
     std::unique_ptr<double[]> matrix;
-    if (process_id == 0)
+    if (rank == 0)
     {
         matrix = std::make_unique<double[]>((dim * 2) * dim);
-
         for (int i = 0; i < dim; i++)
         {
             for (int j = 0; j < dim; j++)
@@ -53,134 +47,131 @@ int main(int argc, char *argv[])
         }
     }
 
-    // Exexcution time
-    double start;
-    if (process_id == 0)
+    // Scatter the matrix into chunks (cyclic)
+    auto chunk = std::make_unique<double[]>(dim * chunk_rows * 2);
+    for (int i = 0; i < chunk_rows; i++)
     {
-        start = MPI_Wtime();
+        MPI_Scatter(matrix.get() + i * (dim * 2) * num_processes, dim * 2, MPI_DOUBLE,
+                    chunk.get() + i * dim * 2, dim * 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 
-    // Partial pivoting
-    if (process_id == 0)
+    // Allocate memory to store pivot row sent by other processes
+    auto pivot_row = std::make_unique<double[]>(dim * 2);
+
+    // Calculate the matrix inverse with gauss-jordan
+    for (int row = 0; row < chunk_rows; row++)
     {
-        double d = 0.0;
-        for (int i = dim; i > 1; --i)
+        // Assign each iteration (current rank) to the belonging process
+        for (int current_rank = 0; current_rank < num_processes; current_rank++)
         {
-            if (matrix[(i - 1) * (dim * 2) + 1] < matrix[i * (dim * 2) + 1])
+            // Calculate the column of the currently evaluated pivot
+            int elim_col = row * num_processes + current_rank;
+
+            // Only execute matrix calculation if the current_rank is equal to the belonging rank
+            if (rank == current_rank)
             {
-                for (int j = 0; j < dim * 2; ++j)
+                // Transform the matrix into a unit matrix
+                double pivot = chunk[row * dim * 2 + elim_col];
+                for (int col = elim_col; col < dim * 2; col++)
                 {
-                    d = matrix[i * (dim * 2) + j];
-                    matrix[i * (dim * 2) + j] = matrix[(i - 1) * (dim * 2) + j];
-                    matrix[(i - 1) * (dim * 2) + j] = d;
+                    chunk[row * dim * 2 + col] /= pivot;
+                }
+
+                // Send the pivot row to the other ranks
+                MPI_Bcast(chunk.get() + row * dim * 2, dim * 2, MPI_DOUBLE, current_rank, MPI_COMM_WORLD);
+
+                // Forward elimination (Elimination for the lower half of the matrix)
+                for (int elim_row = row + 1; elim_row < chunk_rows; elim_row++)
+                {
+                    // Get the ratio for elimination
+                    double scale = chunk[elim_row * dim * 2 + elim_col];
+
+                    // Reducing the lower half of the matrix to be 0
+                    for (int col = elim_col; col < dim * 2; col++)
+                    {
+                        chunk[elim_row * dim * 2 + col] -= chunk[row * dim * 2 + col] * scale;
+                    }
+                }
+
+                // Barrier to wait for all processes to finish the forward elimination
+                MPI_Barrier(MPI_COMM_WORLD);
+
+                // Backward elimination (Elimination for the upper half of the matrix)
+                for (int elim_row = row - 1; elim_row >= 0; elim_row--)
+                {
+                    // Get the ratio for elimination
+                    double scale = chunk[elim_row * dim * 2 + elim_col];
+
+                    // Reducing the upper half of the matrix to be 0
+                    for (int col = elim_col; col < dim * 2; col++)
+                    {
+                        chunk[elim_row * dim * 2 + col] -= chunk[row * dim * 2 + col] * scale;
+                    }
+                }
+            }
+            else
+            {
+                // Receive the pivot from the sending current_rank
+                MPI_Bcast(pivot_row.get(), dim * 2, MPI_DOUBLE, current_rank, MPI_COMM_WORLD);
+
+                int local_start = (rank < current_rank) ? row + 1 : row;
+
+                // Forward elimination (Elimination for the lower half of the matrix)
+                for (int elim_row = local_start; elim_row < chunk_rows; elim_row++)
+                {
+                    // Get the ratio for elimination
+                    double scale = chunk[elim_row * dim * 2 + elim_col];
+
+                    // Reducing the lower half of the matrix to be 0
+                    for (int col = elim_col; col < dim * 2; col++)
+                    {
+                        chunk[elim_row * dim * 2 + col] -= pivot_row[col] * scale;
+                    }
+                }
+
+                // Barrier to wait for all processes to finish the forward elimination
+                MPI_Barrier(MPI_COMM_WORLD);
+
+                // Backward elimination (Elimination for the upper half of the matrix)
+                for (int elim_row = row; elim_row >= 0; elim_row--)
+                {
+                    // Get the ratio for elimination
+                    double scale = chunk[elim_row * dim * 2 + elim_col];
+
+                    // Reducing the upper half of the matrix to be 0
+                    for (int col = elim_col; col < dim * 2; col++)
+                    {
+                        chunk[elim_row * dim * 2 + col] -= pivot_row[col] * scale;
+                    }
                 }
             }
         }
     }
 
-    // Scatter the matrix into chunks
-    for (int i = 0; i < n_rows; i++)
+    // Gather the matrix chunks into a single matrix (cyclic)
+    for (int i = 0; i < chunk_rows; i++)
     {
-        MPI_Scatter(matrix.get() + i * (dim * 2) * num_tasks, dim * 2, MPI_DOUBLE,
-                    m_chunk.get() + i * dim * 2, dim * 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gather(chunk.get() + i * dim * 2, dim * 2, MPI_DOUBLE,
+                   matrix.get() + i * (dim * 2) * num_processes, dim * 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 
-    // Perform gaussian jordan elimination
-    for (int row = 0; row < n_rows; row++) {
-
-        // For every ranks
-        for (int rank = 0; rank < num_tasks; rank++) {
-
-            // Get the pivot index for each row
-            auto global_col = row * num_tasks + rank;
-
-            // If the rank is the same as the task id
-            if (rank == process_id) {
-
-                // Calculate into a unit matrix
-                auto pivot = m_chunk[row * dim * 2 + global_col];
-                for (int col = global_col; col < dim * 2; col++) {
-                    m_chunk[row * dim * 2 + col] /= pivot;
-                }
-
-                // Broadcast the pivot row to other processes
-                MPI_Bcast(m_chunk.get() + row * dim * 2, dim * 2, MPI_DOUBLE, rank, MPI_COMM_WORLD);
-
-                // Eliminate for the local rows
-                for (int elim_row = row + 1; elim_row < n_rows; elim_row++) {
-                    auto scale = m_chunk[elim_row * dim * 2 + global_col];
-
-                    for (int col = global_col; col < dim * 2; col++) {
-                        m_chunk[elim_row * dim * 2 + col] -= m_chunk[row * dim * 2 + col] * scale;
-                    }
-                }
-            } else {
-                // Receive the pivot row from other processes
-                // BUG: The following line causes an error.
-                MPI_Bcast(pivot_row.get(), dim * 2, MPI_DOUBLE, rank, MPI_COMM_WORLD);
-
-                auto local_start = (process_id < rank) ? row + 1 : row;
-
-                // Eliminate for the local rows
-                for (int elim_row = local_start; elim_row < n_rows; elim_row++) {
-                    auto scale = m_chunk[elim_row * dim * 2 + global_col];
-
-                    for (int col = global_col; col < dim * 2; col++) {
-                        m_chunk[elim_row * dim * 2 + col] -= pivot_row[col] * scale;
-                    }
-//                    for (int col = global_col; col >= 0; col--) {
-//                        m_chunk[elim_row * dim * 2 + col] -= pivot_row[col] * scale;
-//                    }
-                }
-            }
-
-        }
-
-    }
-
-    // TODO: Implement the matrix inverse with gauss-jordan
-//     for (int row = 0; row < n_rows; row++)
-//     {
-//         for (int rank = 0; rank < num_tasks; rank++)
-//         {
-//             auto global_col = row * num_tasks + rank;
-//             if (rank == process_id)
-//             {
-//                 auto pivot = m_chunk[row * dim * 2 + global_col];
-//                 for (int col = global_col; col < dim * 2; col++)
-//                 {
-//                     m_chunk[row * dim * 2 + col] /= pivot;
-//                 }
-//             }
-//         }
-//     }
-
-    // Gather the matrix
-    for (int i = 0; i < n_rows; i++)
+    // Print output to a text file
+    if (rank == 0)
     {
-        MPI_Gather(m_chunk.get() + i * dim * 2, dim * 2, MPI_DOUBLE,
-                   matrix.get() + i * (dim * 2) * num_tasks, dim * 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    }
-
-    // Output
-    if (process_id == 0)
-    {
-        double end = MPI_Wtime();
-        std::cout << "Time: " << end - start << '\n';
-        // print the matrix
         for (int i = 0; i < dim; i++)
         {
-            for (int j = 0; j < dim * 2; j++)
+            for (int j = 0; j < dim; j++)
             {
-                std::cout << matrix[i * (dim * 2) + j] << ' ';
+                std::cout << matrix[i * (dim * 2) + dim + j] << " ";
             }
-            std::cout << '\n';
+            std::cout << std::endl;
         }
     }
 
-    // Finalize the MPI environment.
-
+    // Finalize MPI environment
     MPI_Finalize();
 
     return 0;
 }
+
+
