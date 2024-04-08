a106a14038d0cbbc4c6b136025ce416dbfa9dba6
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-04-08 22:30:38 +0700

    feat: initialize cuda

--- src/cuda/cuda.cu
+++ src/cuda/cuda.cu
@@ -1,14 +1,81 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-__global__ void device_hello_world() {
-    printf("Hello world from x.%d y.%d z.%d!\n", threadIdx.x, threadIdx.y, threadIdx.z);
-}
+#include <cuda.h>
+#include <math.h>
 
-int main(void) {
-    dim3 block(2, 2, 2);    // x*y*z <= 1024
-    dim3 grid(1, 1, 1);
-    for (int i = 0; i < 4; i++)
-        device_hello_world<<<grid, block>>>();
-    cudaDeviceSynchronize();
-    return 0;
+using namespace std;
+
+#define blocksize 32
+
+int main()
+{
+	int i = 0, j = 0, n = 0;
+
+    // Get the dimension of the matrix
+    cin >> n;
+
+    // Allocating memory for matrix array
+    double *iL = new double[n*n];
+    double *mat = new double[n*n]();
+
+    // Read matrix from text file
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cin >> mat[i * n + j];
+        }
+    }
+
+	double *mat_c, *id_mat, *id_mat_c;
+	int size = n*n*sizeof(double);
+
+    // Allocate memory on GPU
+	dim3 threadsPerBlock(blocksize, blocksize);
+	dim3 numBlocks((n + blocksize - 1) / blocksize, (n + blocksize - 1) / blocksize);
+	cudaMalloc((void**)&mat_c, size);
+	cudaMalloc((void**)&id_mat_c, size);
+	id_mat = new double[n*n];
+
+    // Initialize the identity matrix
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j) {
+                id_mat[i*n + i] = 1.0;
+            } else {
+                id_mat[i*n + j] = 0.0;
+            }
+		}
+	}
+
+	// Copy data from CPU to GPU
+	cudaMemcpy(mat_c, mat, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(id_mat_c, id_mat, size, cudaMemcpyHostToDevice);
+
+	// Perform the Gauss-Jordan elimination
+    // TODO
+
+	// Copy data from GPU to CPU
+	cudaMemcpy(iL, id_mat_c, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(id_mat, mat_c, size, cudaMemcpyDeviceToHost);
+
+    // Free the memory
+	cudaFree(mat_c);
+	cudaFree(id_mat_c);
+
+    // Print the result
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cout << iL[i * n + j] << " ";
+        }
+        cout << endl;
+    }
+
+	delete[]id_mat;
+	delete[]mat;
+	delete[]iL;
+
+	return 0;
 }
