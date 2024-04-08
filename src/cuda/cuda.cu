abf3f41fa8a15db85c0977a7e00d2c7a8556d2f2
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-04-09 06:29:59 +0700

    feat: cuda.cu optimization v1.0
    Co-Authored-By: Ditra Amadia <60308058+ditramadia@users.noreply.github.com>

--- src/cuda/cuda.cu
+++ src/cuda/cuda.cu
@@ -9,59 +9,130 @@
 
 using namespace std;
 
-#define blocksize 8
+
+__global__ void nodiag_normalize(double *A, double *id_mat, int n, int i){
+ int x = blockIdx.x * blockDim.x + threadIdx.x;
+ int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+ if (x < n && y < n)
+ if (x == i && x!=y){
+  id_mat[x*n + y] /= A[i*n + i];
+  A[x*n + y] /= A[i*n + i];
+ }
+
+}
+
+__global__ void diag_normalize(double *A, double *id_mat, int n, int i){
+ int x = blockIdx.x * blockDim.x + threadIdx.x;
+ int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+ if (x < n && y < n)
+ if (x == y && x == i){
+  id_mat[x*n + y] /= A[i*n + i];
+  A[x*n + y] /= A[i*n + i];
+ }
+}
+
+__global__ void gaussjordan(double *A, double *id_mat, int n, int i)
+{
+ int x = blockIdx.x * blockDim.x + threadIdx.x;
+ int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+ if (x < n && y < n){
+  if (x != i){
+   id_mat[x*n + y] -= id_mat[i*n + y] * A[x*n + i];
+   if (y != i){
+    A[x*n + y] -= A[i*n + y] * A[x*n + i];
+   }
+  }
+ }
+
+}
 
 int main()
 {
-	int i = 0, j = 0, n = 0;
+    int i = 0, j = 0, n = 0, blocksize = 0;
 
     // Get the dimension of the matrix
     cin >> n;
 
+    if (n<8){
+        blocksize = 2;
+    } else if (n<32){
+        blocksize = 4;
+    } else if (n<128){
+        blocksize = 8;
+    } else if (n<512){
+        blocksize = 16;
+    } else if (n<2048){
+        blocksize = 32;
+    } else {
+        blocksize = 64;
+    }
+
     // Allocating memory for matrix array
-    double *mat_res = new double[n*2*n];
-    double *mat = new double[n*2*n];
+    double *iL = new double[n*n];
+    double *mat = new double[n*n]();
 
     // Read matrix from text file
     for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            cin >> mat[i * 2 * n + j];
+        for(j = 0; j < n; ++j) {
+            cin >> mat[i * n + j];
         }
-
-        // Append identity matrix
-        mat[i * 2 * n + n + i] = 1.0;
     }
 
-	double *mat_c;
-	int size = n*2*n*sizeof(double);
+    double *mat_c, *id_mat, *id_mat_c;
+    int size = n*n*sizeof(double);
 
     // Allocate memory on GPU
-	dim3 threadsPerBlock(blocksize, blocksize);
-	dim3 numBlocks((n + blocksize - 1) / blocksize, (n + blocksize - 1) / blocksize);
-	cudaMalloc((void**)&mat_c, size);
+    dim3 threadsPerBlock(blocksize, blocksize);
+    dim3 numBlocks((n) / blocksize, (n) / blocksize);
+    cudaMalloc((void**)&mat_c, size);
+    cudaMalloc((void**)&id_mat_c, size);
+    id_mat = new double[n*n];
 
-	// Copy data from CPU to GPU
-	cudaMemcpy(mat_c, mat, size, cudaMemcpyHostToDevice);
 
-	// Perform the Gauss-Jordan elimination
-    // TODO
+    // Initialize the identity matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                id_mat[i*n + i] = 1.0;
+            } else {
+                id_mat[i*n + j] = 0.0;
+            }
+        }
+    }
 
-	// Copy data from GPU to CPU
-	cudaMemcpy(mat_res, mat_c, size, cudaMemcpyDeviceToHost);
+    // Copy data from CPU to GPU
+    cudaMemcpy(mat_c, mat, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(id_mat_c, id_mat, size, cudaMemcpyHostToDevice);
 
-    // Free the memory
-	cudaFree(mat_c);
+    // Perform the Gauss-Jordan elimination
+    for (int i = 0; i < n; i++){
+        nodiag_normalize <<<numBlocks, threadsPerBlock >>>(mat_c, id_mat_c, n, i);
+        diag_normalize <<<numBlocks, threadsPerBlock >>>(mat_c, id_mat_c, n, i);
+        gaussjordan <<<numBlocks, threadsPerBlock >>>(mat_c, id_mat_c, n, i);
+    }
 
-    // Print the result
+    //copy data from GPU to CPU
+    cudaMemcpy(iL, id_mat_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(id_mat, mat_c, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(mat_c);
+    cudaFree(id_mat_c);
+
+    //print the result
     for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            cout << mat_res[i * 2 * n + n + j] << " ";
+        for(j = 0; j < n; ++j)
+        {
+            cout << iL[i * n + j] << " ";
         }
         cout << endl;
     }
 
-	delete[]mat;
-	delete[]mat_res;
+    delete[]id_mat;
+    delete[]mat;
+    delete[]iL;
 
-	return 0;
+    return 0;
 }
